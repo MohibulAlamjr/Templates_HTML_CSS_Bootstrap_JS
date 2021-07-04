80412a6331f0581903336c7a8c30496f1d1b556f
Mohibul Alam <mohibulalamjr@gmail.com>
2021-07-05 01:33:54 +0600

    templates

---
+++ SKOKVO/js/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>hi Lochost</h1>
+
+
+
+
+
+
+</body>
+</html>
---
+++ Skokov/Asset/JS/main.js
@@ -0,0 +1,14 @@
+new WOW().init();
+ 
+ var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+     typeSpeed:200,
+     backSpeed:40,
+     loop: true,
+     loopCount:10,
+      showCursor:true,
+     cursorChar:"|",
+  });
+$(document).ready(function(){
+    $(".splitchar").splitchar();
+});
